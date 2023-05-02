d9ddbe22650a6c10341512fdcd081f0fa80ae67b
Mustafa Sungur Öztürk <mustafasungur.ozturkk@gmail.com>
2023-05-02 16:23:42 +0300

    create project

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// ejs tamplate engine
+app.set("view engine", "ejs");
+
+// static files middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.listen(port, () => {
+  console.log(`port:${port}`);
+});
