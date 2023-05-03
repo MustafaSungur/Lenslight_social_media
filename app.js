306c64a35ac74530741778d151a394053dd342f2
Mustafa Sungur Öztürk <mustafasungur.ozturkk@gmail.com>
2023-05-03 22:38:07 +0300

    created controllers,models,routes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
-
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
 dotenv.config();
 
 // connection to the db
@@ -15,13 +16,12 @@ app.set("view engine", "ejs");
 
 // static files middleware
 app.use(express.static("public"));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+// routes
+app.use("/", pageRoute);
+app.use("/photo", photoRoute);
+
 app.listen(port, () => {
   console.log(`port:${port}`);
 });
